e691398a8da0a1a2556d17f568d8f0cedd6b3840
Alex Thanas <greek313@gmail.com>
2025-12-09 17:30:30 -0600

    refactor partially done, app posts to Mongo, need to add middleware and lint styling

--- blogbackend/index.js
+++ blogbackend/index.js
@@ -1,38 +1,9 @@
 const config = require('./utils/config')
-const express = require ('express')
-const mongoose = require('mongoose')
+const app = require('./app')
+const logger = require('./utils/logger')
 
-const app = express()
-
-const blogSchema = mongoose.Schema({
-    title: String,
-    content: String,
-    url: String,
-    like: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-
-mongoose.connect(config.MONGODB_URI , { family:4})
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-    Blog.find({}).then(blogs => {
-        response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request,response) => {
-    const blog = new Blog(request.body)
-
-    blog.save().then(savedBlog => {
-        response.status(201).json(savedBlog)
-    })
-})
 
 
 app.listen(config.PORT, () => {
-    console.log(`Server running on port ${config.PORT}`);
+    logger.info(`Server running on port ${config.PORT}`);
 })
